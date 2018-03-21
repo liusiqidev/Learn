3d74776c101503411dac4ef531ec64a70bbe764b
liusiqidev <862612899@qq.com>
2018-03-21 08:33:49 +0800

    测试天气接口

--- app/src/main/java/com/lsq/learn/weather/WeatherMainAct.java
+++ app/src/main/java/com/lsq/learn/weather/WeatherMainAct.java
@@ -2,15 +2,39 @@ package com.lsq.learn.weather;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.lsq.learn.BaseActivity;
 import com.lsq.learn.R;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
 
 /**
  * Created by lsq on 2018/3/18.
@@ -25,17 +49,48 @@ public class WeatherMainAct extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_weather_main);
         ButterKnife.bind(this);
-        new Retrofit.Builder()
-                .baseUrl("http://jisutqybmf.market.alicloudapi.com/")
-                .addConverterFactory()
-                .build()
-                .create(api.class)
-                .getWeather("北京")
-        ;
+
     }
 
     @OnClick(R.id.bt_weather)
     public void onViewClicked() {
+        OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+        builder.readTimeout(10, TimeUnit.SECONDS);
+        builder.connectTimeout(9, TimeUnit.SECONDS);
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.addInterceptor(httpLoggingInterceptor);
 
+        new Retrofit.Builder()
+                .baseUrl("http://jisutqybmf.market.alicloudapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(builder.build())
+                .build()
+                .create(api.class)
+                .getWeather("北京")
+                .subscribeOn(Schedulers.io())
+                .flatMap(new Function<weatherBean, ObservableSource<weatherBean.ResultBean.HourlyBean>>() {
+                    @Override
+                    public ObservableSource<weatherBean.ResultBean.HourlyBean> apply(weatherBean weatherBean) throws Exception {
+                        List<weatherBean.ResultBean.HourlyBean> hourlyBeanList = weatherBean.getResult().getHourly();
+                        return Observable.fromIterable(hourlyBeanList);
+                    }
+                })
+                .filter(new Predicate<weatherBean.ResultBean.HourlyBean>() {
+                    @Override
+                    public boolean test(weatherBean.ResultBean.HourlyBean hourlyBean) throws Exception {
+                        return true;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<weatherBean.ResultBean.HourlyBean>() {
+                    @Override
+                    public void accept(weatherBean.ResultBean.HourlyBean hourlyBean) throws Exception {
+                        Log.e("hhh", "accept: " +hourlyBean.getTime());
+                        Log.e("hhh", "accept: " +hourlyBean.getWeather());
+                        tvWeather.setText(tvWeather.getText()+"\n"+hourlyBean.getTime()+hourlyBean.getWeather());
+                    }
+                });
     }
 }
--- app/src/main/java/com/lsq/learn/weather/api.java
+++ app/src/main/java/com/lsq/learn/weather/api.java
@@ -1,17 +1,20 @@
 package com.lsq.learn.weather;
 
+import io.reactivex.Observable;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Part;
-import rx.Observable;
+import retrofit2.http.HEAD;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
 
 /**
  * Created by lsq on 18-3-19.
  */
 
 public interface api {
-    @FormUrlEncoded
-    @GET("/weather/query")
-    Observable<weatherBean> getWeather(@Field("city") String city);
+    @Headers("Authorization:APPCODE 847b64712afb4b4cab0e6deb0b0af04f")
+    @GET("weather/query")
+    Observable<weatherBean> getWeather(@Query("city") String city);
 }
